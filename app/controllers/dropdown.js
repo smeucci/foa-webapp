909804d9bcc627147e7c6b137065157add9090c6
Saverio <s.meucci91@gmail.com>
2017-01-20 21:10:19 +0100

    controller dropdown now process the data to pass to db functions

--- app/controllers/dropdown.js
+++ app/controllers/dropdown.js
@@ -11,21 +11,23 @@ module.exports = {
 }
 
 function getMakers (req, res) {
-    db.getMakers(function (err, makers) {
+    db.selectMakers(function (err, makers) {
         if (err) { callback(err); return; }
         res.status(202).json(makers);
     })
 }
 
 function getModels (req, res) {
-    db.getModels(req, function (err, models) {
+    var data = {maker: req.body.maker}
+    db.selectModels(data, function (err, models) {
         if (err) { callback(err); return; }
         res.status(202).json(models)
     })
 }
 
 function getOS (req, res) {
-    db.getOS(req, function (err, os) {
+    var data = {maker: req.body.maker, model: req.body.model}
+    db.selectOS(data, function (err, os) {
         if (err) { callback(err); return; }
         res.status(202).json(os)
     })
