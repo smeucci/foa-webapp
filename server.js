b182bb7ea4f9f00ac863f06cd2cfd328d815d0a9
Saverio <s.meucci91@gmail.com>
2017-01-21 13:28:45 +0100

    basic file uploader

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var multer = require('multer')
 var unique = require('array-unique');
 var port = process.env.PORT || 3000
 
@@ -9,6 +10,7 @@ var port = process.env.PORT || 3000
 var dropdown = require(__dirname + '/app/controllers/dropdown.js');
 
 // settings
+app.use(multer({dest: './app/uploads/'}).any());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
@@ -20,7 +22,12 @@ app.get('/', function (req, res) {
 app.get('/makers', dropdown.getMakers);
 app.post('/models', dropdown.getModels);
 app.post('/os', dropdown.getOS);
+app.post('/upload', function (req, res) {
+    console.log(req.files)
+})
 
 app.listen(port, function () {
     console.log('Listening on port ' + port + '...');
 });
+
+module.exports = app
