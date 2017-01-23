2f2c2e99d1baa3dfa7d2de551651c9b1b1956087
Saverio <s.meucci91@gmail.com>
2017-01-23 19:19:40 +0100

    new sintax for function, easier to switch from async/await to asyncawait

--- app/controllers/dropdown.js
+++ app/controllers/dropdown.js
@@ -6,18 +6,18 @@
 var db = require(__dirname + '/../models/db.js');
 
 // functions
-async function getMakers (req, res) {
+var getMakers = async function(req, res) {
     var makers = await db.selectMakers();
     res.status(202).json(makers);
 }
 
-async function getModels (req, res) {
+var getModels = async function (req, res) {
     var data = { maker: req.body.maker };
     var models = await db.selectModels(data);
     res.status(202).json(models);
 }
 
-async function getOS (req, res) {
+var getOS = async function (req, res) {
     var data = { maker: req.body.maker, model: req.body.model };
     var os = await db.selectOS(data);
     res.status(202).json(os);
