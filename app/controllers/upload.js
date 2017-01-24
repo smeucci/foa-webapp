3246f42d7bc61b43ea2ceeec2cecdc9ef6abb294
Saverio <s.meucci91@gmail.com>
2017-01-24 17:15:58 +0100

    upload controller (to refactor)

---
+++ app/controllers/upload.js
@@ -0,0 +1,35 @@
+// =========
+// upload.js
+// =========
+
+// require
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+
+// functions
+function upload (req, res) {
+  var form = new formidable.IncomingForm();
+  form.multiples = true;
+  form.uploadDir = path.join(__dirname, '../uploads');
+
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name), function () {});
+  });
+
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  form.parse(req);
+
+}
+
+// exports
+module.exports = {
+    upload
+}
