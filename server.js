4a378dad6adde70aea4caa1e7255ddb2bb52d50b
Saverio <s.meucci91@gmail.com>
2017-01-24 17:15:42 +0100

    now using get for dropdown controllers functions

--- server.js
+++ server.js
@@ -6,13 +6,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var multer = require('multer');
 var unique = require('array-unique');
-var dropdown = require(__dirname + '/app/controllers/dropdown.js');
+var dropdown = require(__dirname + '/app/controllers/dropdown');
+var upload = require(__dirname + '/app/controllers/upload');
 var port = process.env.PORT || 3000;
 
 // settings
-app.use(multer({dest: __dirname + '/app/uploads/'}).any());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
@@ -21,12 +20,10 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/app/views/index.html')
 });
 
-app.get('/makers', dropdown.getMakers);
-app.post('/models', dropdown.getModels);
-app.post('/os', dropdown.getOS);
-app.post('/upload', function (req, res) {
-    console.log(req.files)
-});
+app.get('/brands', dropdown.getBrands);
+app.get('/models', dropdown.getModels);
+app.get('/os', dropdown.getOS);
+app.post('/upload', upload.upload);
 
 app.listen(port, function () {
     console.log('Listening on port ' + port + '...');
