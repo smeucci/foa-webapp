e1f0a952a07fc9a1ea2479223b6992515f962a0e
Saverio <s.meucci91@gmail.com>
2017-01-25 16:54:51 +0100

    upload goes in utils

---
+++ app/utils/upload.js
@@ -0,0 +1,37 @@
+// =========
+// upload.js
+// =========
+
+// require
+var fs = require('fs');
+var path = require('path');
+var formidable = require('formidable');
+
+// functions
+function upload (req) {
+    return new Promise ( function (resolve, reject) {
+        var form = new formidable.IncomingForm();
+        form.multiples = true;
+        form.uploadDir = path.join(__dirname, '../uploads');
+
+        form.on('file', function(field, file) {
+            fs.rename(file.path, path.join(form.uploadDir, file.name), function () {});
+        });
+
+        form.on('error', function(err) {
+            console.log('An error has occured: ' + err);
+            return resolve(false)
+        });
+
+        form.on('end', function() {
+            return resolve(true)
+        });
+
+        form.parse(req);
+    })
+}
+
+// exports
+module.exports = {
+    upload
+}
