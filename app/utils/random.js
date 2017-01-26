e63b8538a49295952d406734e521c71c9ebe6ae8
Saverio <s.meucci91@gmail.com>
2017-01-26 15:42:54 +0100

    generate random alphanumeric string of selected lenght

---
+++ app/utils/random.js
@@ -0,0 +1,17 @@
+// ========
+// utils.js
+// ========
+
+// functions
+const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function generate (length) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+    return result;
+}
+
+// exports
+module.exports = {
+    generate
+}
