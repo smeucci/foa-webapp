d413a93bb5c5f3f7d419cd5c3dd3d2669ec23516
Saverio <s.meucci91@gmail.com>
2017-01-31 19:45:06 +0100

    upload now returns the filenames of the uploaded files

--- app/utils/upload.js
+++ app/utils/upload.js
@@ -12,11 +12,12 @@ function upload (req, folder) {
     return new Promise ( function (resolve, reject) {
         var form = new formidable.IncomingForm();
         form.multiples = true;
-        form.uploadDir = path.join(__dirname, '../uploads/', folder);
+        form.uploadDir = path.join(uploadsDir(), folder);
         fs.mkdirSync(form.uploadDir);
-
+        form.filenames = [];
         form.on('file', function(field, file) {
             fs.rename(file.path, path.join(form.uploadDir, file.name), function () {});
+            form.filenames.push({filename: file.name});
         });
 
         form.on('field', function(field, value) {
@@ -29,14 +30,19 @@ function upload (req, folder) {
         });
 
         form.on('end', function() {
-            return resolve({success: true, class: form.fields});
+            return resolve({success: true, class: form.fields, filenames: form.filenames});
         });
 
         form.parse(req);
     })
 }
 
+function uploadsDir () {
+    return path.join(__dirname, '/../uploads/');
+}
+
 // exports
 module.exports = {
-    upload
+    upload,
+    uploadsDir
 }
