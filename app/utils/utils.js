f01741798e9b6c005f2976c38ab472fb0de05142
Saverio <s.meucci91@gmail.com>
2017-02-06 14:25:38 +0100

    select max likelihood among all classes for each test video, for auto

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -8,7 +8,7 @@ var fs = require('fs-extra');
 var upload = require(path.join(__dirname, '/../utils/upload'));
 
 // functions
-function isEmpty(obj) {
+function isEmpty (obj) {
     if (obj == null) return true;
     if (obj.length > 0)    return false;
     if (obj.length === 0)  return true;
@@ -19,7 +19,36 @@ function isEmpty(obj) {
     return true;
 }
 
+function maxLikelihood (data, num) {
+    var max = Array(num).fill(0);
+    var best = Array(num).fill(0);
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < data[i].results.length; j++) {
+            if (data[i].results[j].likelihood > max[j]) {
+                max[j] = data[i].results[j].likelihood;
+                best[j] = i;
+            }
+        }
+    }
+    return parseMaxLikehood(num, data, best);
+}
+
+function parseMaxLikehood (num, data, best) {
+    var likelihoods = {results: []};
+    for (var i = 0; i < num; i++) {
+        var likelihood = {};
+        var res = data[best[i]].results[i];
+        likelihood.filename = res.filename;
+        likelihood.likelihood = res.likelihood;
+        likelihood.loglikelihood = res.loglikelihood;
+        likelihood.class = res.class;
+        likelihoods.results.push(likelihood);
+    }
+    return likelihoods;
+}
+
 // exports
 module.exports = {
-    isEmpty
+    isEmpty,
+    maxLikelihood
 }
